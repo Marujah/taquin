b885144b560b5a90c7a4f14e5eae64addf665973
Marouen Mhiri <marujah@gmail.com>
2023-12-21 23:34:31 +0100

    fix public dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/taquin',
+  base: '/taquin/',
+  publicDir: '/taquin/',
   plugins: [vue()],
 })
